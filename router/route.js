ea45bdd4de10a38c744e4fa5cb752b5f88f6ab50
John Paderog <91768994+johngabriel613@users.noreply.github.com>
2024-01-28 19:39:04 +0800

    add route for cron job

--- router/route.js
+++ router/route.js
@@ -3,6 +3,10 @@ import gtfs from "../controller/route.controller.js";
 
 export const router = Router();
 
+router.get('/initialize', (req, res) => {
+  res.json(200).json({status: 'server is currently running'})
+});
+
 router.get('/routes', gtfs.getAllRoutes)
 router.get('/routes/:id', gtfs.getRoutesById)
 router.get('/search', gtfs.searchRoute)
